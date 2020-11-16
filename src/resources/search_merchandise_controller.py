d5728f6a1f4ab096bbe55996e40e8e27e9d6ce54
Yen-Tse Liao <s88037zz@gmail.com>
2020-11-16 12:23:06 +0800

    using api to search productis ok, but it only return result of searching in shoppe. It will improve to return multiple platform when architectture in utilties is defined.

--- src/resources/search_merchandise_controller.py
+++ src/resources/search_merchandise_controller.py
@@ -1,11 +1,15 @@
 from flask_restful import Resource, request
+from src.utilities.scraper.shoppe_data_controller import ShoppeDataController
 
 
 class SearchMerchandiseController(Resource):
     def get(self):
-        key_word = request.args.get("key_word")
-
+        key_words = request.args.get("key_word").split(' ')
+        scraper = ShoppeDataController()
+        scraper.search(key_words)
+        result = scraper.result
         return {
             "message": "",
-            "key_word": key_word
+            "key_words": key_words,
+            "result": result
         },  200
